3d6bdc32d253f132eda629daaf74280592d5a395
Denir <denir-junior19@live.com>
2021-01-17 13:35:06 -0300

    naster(): polymorphism

--- index.js
+++ index.js
@@ -3,9 +3,13 @@ import { Gerente } from './Funcionario/Gerente.js';
 import { SistemaAutenticacao } from './SistemaAutenticacao.js';
 
 const diretor = new Diretor("Denir", 10000, 421410355214);
+diretor.cadastrarSenha('123456');
 
 const gerente = new Gerente("junior", 5000, 555510355214);
 gerente.cadastrarSenha("123");
 
-const estaLogado = SistemaAutenticacao.login(gerente, "123");
-console.log(estaLogado);
+const diretorEstaLogado = SistemaAutenticacao.login(diretor, "123456");
+const gerenteEstaLogado = SistemaAutenticacao.login(gerente, "123");
+
+console.log('DIRETOR', diretorEstaLogado);
+console.log('GERENTE', gerenteEstaLogado);
