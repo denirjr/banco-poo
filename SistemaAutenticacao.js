159a0942ceb7ebbf0dfe3686fbac0fb3af87c8fd
Denir <denir-junior19@live.com>
2021-01-17 14:11:15 -0300

    (master): interfaces to conect polymorphism concept

--- SistemaAutenticacao.js
+++ SistemaAutenticacao.js
@@ -1,5 +1,21 @@
+/**
+ * Ser autenticavel significa ter um método "autenticar"
+ *
+ * @export
+ * @class SistemaAutenticacao
+ */
 export class SistemaAutenticacao {
-  static login(funcionario, senha) {
-    return funcionario.senha === senha;
+  static login(autenticavel, senha) {
+    if (SistemaAutenticacao.ehAutenticavel(autenticavel)) {
+      return autenticavel.autenticar(senha);
+    }
+    return false;
+  }
+
+  static ehAutenticavel(autenticavel) {
+    return (
+      "autenticar" in autenticavel &&
+      autenticavel.autenticar instanceof Function
+    );
   }
 }
--- index.js
+++ index.js
@@ -1,15 +1,20 @@
-import { Diretor } from './Funcionario/Diretor.js';
-import { Gerente } from './Funcionario/Gerente.js';
-import { SistemaAutenticacao } from './SistemaAutenticacao.js';
+import { Cliente } from "./Cliente.js";
+import { Diretor } from "./Funcionario/Diretor.js";
+import { Gerente } from "./Funcionario/Gerente.js";
+import { SistemaAutenticacao } from "./SistemaAutenticacao.js";
 
 const diretor = new Diretor("Denir", 10000, 421410355214);
-diretor.cadastrarSenha('123456');
+diretor.cadastrarSenha("123456");
 
 const gerente = new Gerente("junior", 5000, 555510355214);
 gerente.cadastrarSenha("123");
 
+const cliente = new Cliente("Lais", 42141093848, 123);
+
 const diretorEstaLogado = SistemaAutenticacao.login(diretor, "123456");
 const gerenteEstaLogado = SistemaAutenticacao.login(gerente, "123");
+const clienteEstaLogado = SistemaAutenticacao.login(cliente, "123");
 
-console.log('DIRETOR', diretorEstaLogado);
-console.log('GERENTE', gerenteEstaLogado);
+console.log("DIRETOR", diretorEstaLogado);
+console.log("GERENTE", gerenteEstaLogado);
+console.log("CLIENTE", clienteEstaLogado);
